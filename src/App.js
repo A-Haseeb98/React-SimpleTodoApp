d89597459c7ad7369412a3569165069b811ddafa
A-Haseeb98 <76472496+A-Haseeb98@users.noreply.github.com>
2021-09-16 09:54:24 +0500

    add edit functionality

--- src/App.js
+++ src/App.js
@@ -1,51 +1,61 @@
-import './App.css';
-import { Button, Input } from './Component'
-import { useState } from 'react'
-
+import "./App.css";
+import { Button, Input } from "./Component";
+import { useState } from "react";
 
 function App() {
-
-  const [value, setValue] = useState('Haseeb')
-  const [todos, setTodo] = useState(['1', '2', '3'])
+  const [value, setValue] = useState("Haseeb");
+  const [todos, setTodo] = useState(["task 1", " task 2", " task 3"]);
 
   let editTodo = (key) => {
-    console.log('h w', key)
-  }
+    let edit = prompt("enter Updated Value", todos[key]);
+    let arr = [...todos];
+    arr[key] = edit;
+    console.log(arr[key]);
+    setTodo(arr);
+  };
 
   let deleteTodo = (key) => {
-    console.log('h w', key)
+    console.log("h w", key);
 
-    let arr = [...todos]
-    arr.splice(key, 1)
-    setTodo(arr)
-  }
+    let arr = [...todos];
+    arr.splice(key, 1);
+    setTodo(arr);
+  };
 
   let deleteAllTodos = () => {
-    setTodo([])
-  }
+    setTodo([]);
+  };
 
   let addTodo = () => {
-    let arr = [...todos]
-    arr.push(value)
-    setTodo(arr)
-    console.log(todos)
-  }
+    let arr = [...todos];
+    arr.push(value);
+    setTodo(arr);
+    console.log(todos);
+  };
   return (
-    <div className='container'>
-<h1>TODO APP </h1>
-      
-      <Input  placeholder='Enter Here' onChange={(e) => setValue(e.target.value)} type='text' />
+    <div className="container">
+      <h1>TODO APP </h1>
+
+      <Input
+        placeholder="Enter Here"
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+      />
       <Button onClick={addTodo}>Add</Button>
       <Button onClick={deleteAllTodos}>Delete All</Button>
 
-      <ul className='todoList'>
+      <ul className="todoList">
         {todos.map((value, index) => {
           return (
-            <li key={index}> <small className='todo'>{value}</small>   <Button onClick={() => editTodo(index)}>Edit</Button> <Button onClick={() => deleteTodo(index)}>Delete</Button> </li>
-          )
+            <li key={index}>
+              {" "}
+              <small className="todo">{value}</small>{" "}
+              <Button onClick={() => editTodo(index)}>Edit</Button>{" "}
+              <Button onClick={() => deleteTodo(index)}>Delete</Button>{" "}
+            </li>
+          );
         })}
       </ul>
-
     </div>
   );
 }
